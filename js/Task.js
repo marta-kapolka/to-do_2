8236eb416f863c9ea4b098da789279a2e2a2e849
marta-kapolka <mkapolka@gmail.com>
2021-07-31 21:15:36 +0200

    feat: create class Task

--- js/Task.js
+++ js/Task.js
@@ -1,1 +1,16 @@
-export class Task {}
+export class Task {
+  constructor(id, value) {
+    this._id = id;
+    this._value = value;
+    this._complete = false;
+    this._important = false;
+  }
+
+  changeCompletion() {
+    this._complete = !this._complete;
+  }
+
+  changeImportance() {
+    this._important = !this._important;
+  }
+}
