643bb699b126cf2b556c253872c5083bff77a83f
marta-kapolka <mkapolka@gmail.com>
2021-07-31 21:18:01 +0200

    feat: create class ListModel with methods: add, remove, find, filter, changeImportance, changeCompletion

--- js/ListModel.js
+++ js/ListModel.js
@@ -1,3 +1,53 @@
 import { Task } from "./Task.js";
 
-export class ListModel {}
+export class ListModel {
+  constructor() {
+    this._taskList = [];
+    this._currentId = 1;
+  }
+
+  addTask(value) {
+    this.setCurrentId();
+    this._taskList.push(new Task(this._currentId, value));
+  }
+
+  setCurrentId() {
+    if (this._taskList.length === 0) this._currentId = 1;
+    else this._currentId++;
+  }
+
+  findTaskById(id) {
+    // return task with matching id
+    return this._taskList.indexOf(
+      this._taskList.find((item) => item._id === id)
+    );
+  }
+
+  findTasksByValue(text) {
+    // return array of tasks which include text
+    return this._taskList.filter((item) => item._value.includes(text));
+  }
+
+  filterTasks(property, value) {
+    return this._taskList.filter((item) => item[`_${property}`] === value);
+  }
+
+  //   sortTasks() {    APPLY IN VIEW WHEN RENDERING..?
+  //     return [
+  //       ...this.filterTasks("important", true),
+  //       ...this.filterTasks("important", false),
+  //     ];
+  //   }
+
+  removeTask(id) {
+    this._taskList.splice(this.findTaskById(id), 1);
+  }
+
+  changeTaskImportance(id) {
+    this._taskList[this.findTaskById(id)].changeImportance();
+  }
+
+  changeTaskCompletion(id) {
+    this._taskList[this.findTaskById(id)].changeCompletion();
+  }
+}
