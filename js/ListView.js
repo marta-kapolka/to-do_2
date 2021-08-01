f3e0a298edc9bb62665e4e1f044a6edef638843f
marta-kapolka <mkapolka@gmail.com>
2021-08-01 11:51:51 +0200

    feat: create class ListView with basic methods: create element, select element, get input value, clear input

--- js/ListView.js
+++ js/ListView.js
@@ -1,1 +1,25 @@
-export class ListView {}
+export class ListView {
+  constructor() {
+    this._taskInput = this.selectHtmlElement(".add-task__input--js");
+    this._taskForm = this.selectHtmlElement(".add-task--js");
+  }
+
+  createHtmlElement(tag, ...className) {
+    const element = document.createElement(tag);
+    className.forEach((item) => element.classList.add(item));
+    return element;
+  }
+
+  selectHtmlElement(selector) {
+    const element = document.querySelector(selector);
+    return element;
+  }
+
+  getTaskInputValue() {
+    console.log(this._taskInput.value);
+  }
+
+  deleteInputValue(input) {
+    input.value = "";
+  }
+}
--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ import { ListView } from "./ListView.js";
 const app = new ListController(new ListModel(), new ListView());
 
 console.log(app);
+app._listModel.addTask("cosik");
+app._listModel.addTask("cosik jeszcze");
+app._listModel.addTask("i następne");
+app._listModel.addTask("i jeszcze kolejne");
+
+app._listView.createHtmlElement("div", "ąśdasdas", "sdfasfsdf");
+app._listView.selectHtmlElement(".list");
+app._listView.getTaskInputValue();
